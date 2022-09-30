67bae548067f3fd1c7e0116b4835fade17295041
GabrielEVT <gabrielsoaresevt@gmail.com>
2022-09-30 13:51:40 -0300

    Header da página finalizado

--- js/script.js
+++ js/script.js
@@ -40,3 +40,18 @@ const mobileNavBar = new MobileNavBar(
     ".header__navigation-menu__list__links__item"
 );
 mobileNavBar.init();
+
+let searchBarIcon = document.getElementsByClassName("header__search-bar-icon");
+let i;
+
+for (i = 0; i < searchBarIcon.length; i++) {
+  searchBarIcon[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var searchBar = document.querySelector(".search-bar-mobile");
+    if (searchBar.style.maxHeight) {
+      searchBar.style.maxHeight = null;
+    } else {
+      searchBar.style.maxHeight = "45px";
+    } 
+  });
+}
