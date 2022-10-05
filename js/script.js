c830926bfa7e3fcdf24c7a94f3c390f691550f18
GabrielEVT <gabrielsoaresevt@gmail.com>
2022-10-04 22:10:42 -0300

    Finalização da página de cadastro

--- js/script.js
+++ js/script.js
@@ -55,3 +55,11 @@ for (i = 0; i < searchBarIcon.length; i++) {
     } 
   });
 }
+
+setTimeout(function () {
+    console.log("Funcionou!");
+    let viewheight = $(window).height();
+    let viewwidth = $(window).width();
+    let viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+}, 300);
