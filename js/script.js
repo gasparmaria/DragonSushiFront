235d6c78cf1fbae697b488b78bfda6c6d11900f6
GabrielEVT <gabrielsoaresevt@gmail.com>
2022-10-10 18:52:08 -0300

    Página de lançar pedido finalizada

--- js/script.js
+++ js/script.js
@@ -1,64 +1,83 @@
-class MobileNavBar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
-        this.overflowY = true;
-        this.handleClick = this.handleClick.bind(this);
-    }
-    hideScrollBar() {
-        document.body.style.overflowY = this.overflowY ? "hidden" : "visible";
-        this.overflowY = !this.overflowY;
-    }
-    hideNavList() {
-        this.navLinks.forEach((link) => {
-            link.addEventListener("click", () => {
-                document.body.style.overflowY = "visible";
-                this.navList.classList.remove(this.activeClass);
-            });
-        });
-    }
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.hideNavList();
-        this.hideScrollBar();
-    }
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-    init() {
-        if(this.mobileMenu) {
-            this.addClickEvent();
+$(document).ready(function(){
+    class MobileNavBar {
+        constructor(mobileMenu, navList, navLinks) {
+            this.mobileMenu = document.querySelector(mobileMenu);
+            this.navList = document.querySelector(navList);
+            this.navLinks = document.querySelectorAll(navLinks);
+            this.activeClass = "active";
+            this.overflowY = true;
+            this.handleClick = this.handleClick.bind(this);
         }
-        return this;
+        hideScrollBar() {
+            document.body.style.overflowY = this.overflowY ? "hidden" : "visible";
+            this.overflowY = !this.overflowY;
+        }
+        hideNavList() {
+            this.navLinks.forEach((link) => {
+                link.addEventListener("click", () => {
+                    document.body.style.overflowY = "visible";
+                    this.navList.classList.remove(this.activeClass);
+                });
+            });
+        }
+        handleClick() {
+            this.navList.classList.toggle(this.activeClass);
+            this.hideNavList();
+            this.hideScrollBar();
+        }
+        addClickEvent() {
+            this.mobileMenu.addEventListener("click", this.handleClick);
+        }
+        init() {
+            if(this.mobileMenu) {
+                this.addClickEvent();
+            }
+            return this;
+        }
     }
-}
-const mobileNavBar = new MobileNavBar(
-    ".header__mobile-menu",
-    ".header__navigation-menu",
-    ".header__navigation-menu__list__links__item"
-);
-mobileNavBar.init();
+    const mobileNavBar = new MobileNavBar(
+        ".header__mobile-menu",
+        ".header__navigation-menu",
+        ".header__navigation-menu__list__links__item"
+    );
+    mobileNavBar.init();
+    
+    let searchBarIcon = document.getElementsByClassName("header__search-bar-icon");
+    let i;
+    
+    for (i = 0; i < searchBarIcon.length; i++) {
+      searchBarIcon[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var searchBar = document.querySelector(".search-bar-mobile");
+        if (searchBar.style.maxHeight) {
+          searchBar.style.maxHeight = null;
+        } else {
+          searchBar.style.maxHeight = "45px";
+        } 
+      });
+    }
+    
+    setTimeout(function () {
+        let viewheight = $(window).height();
+        let viewwidth = $(window).width();
+        let viewport = document.querySelector("meta[name=viewport]");
+        viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+    }, 300);
 
-let searchBarIcon = document.getElementsByClassName("header__search-bar-icon");
-let i;
+    $(".target").hide();
+    $("#div1").show();
 
-for (i = 0; i < searchBarIcon.length; i++) {
-  searchBarIcon[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var searchBar = document.querySelector(".search-bar-mobile");
-    if (searchBar.style.maxHeight) {
-      searchBar.style.maxHeight = null;
-    } else {
-      searchBar.style.maxHeight = "45px";
-    } 
-  });
-}
+    $(".single").click(function(){
+        $(".target").hide()
+        $("#div"+$(this).attr("target")).show();
+    });
 
-setTimeout(function () {
-    let viewheight = $(window).height();
-    let viewwidth = $(window).width();
-    let viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
-}, 300);
+    let showRestaurantMenuItem = document.querySelectorAll(".choose-dishe__menu_item");
+    showRestaurantMenuItem.forEach(item => {
+        item.addEventListener("click", function() {
+            showRestaurantMenuItem.forEach(item=>item.classList.remove("accent-background"));
+            item.classList.add("accent-background");
+        });
+    }); 
+});
+
