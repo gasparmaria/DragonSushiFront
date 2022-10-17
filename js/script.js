3944927a90aaedb5b574120cdbec3fca3109b38b
GabrielEVT <gabrielsoaresevt@gmail.com>
2022-10-17 10:27:05 -0300

    Finalização páginas do funcionário

--- js/script.js
+++ js/script.js
@@ -79,5 +79,168 @@ $(document).ready(function(){
             item.classList.add("accent-background");
         });
     }); 
+
+    // ---------------------------------------------------------------------------------------
+
+    function AumentaPerc() {
+
+        var campo = document.getElementById("percentual")
+        var preco = document.getElementById("price")
+        var list1 = document.getElementById("list1")
+    
+        
+        campo.value = parseInt(campo.value) + 1;
+        preco.value = parseInt(preco.value) + 18;
+        list1.value = parseInt(list1.value) + 1;
+    
+    }
+    
+    function DiminuiPerc() {
+    
+        var campo = document.getElementById("percentual")
+        var preco = document.getElementById("price")
+        var list1 = document.getElementById("list1")
+        
+        
+        campo.value= parseInt(campo.value) - 1;
+        preco.value= parseInt(preco.value) - 18;
+        list1.value = parseInt(list1.value) - 1;
+    }
+
+    function AumentaPerc2() {
+
+        var campo2 = document.getElementById("percentual2")
+        var preco2 = document.getElementById("price2")
+        var list2 = document.getElementById("list2")
+    
+        campo2.value= parseInt(campo2.value) + 1;
+        preco2.value= parseInt(preco2.value) + 23;
+        list2.value = parseInt(list2.value) + 1;
+    }
+
+    function DiminuiPerc2() {
+    
+        var campo2 = document.getElementById("percentual2")
+        var preco2 = document.getElementById("price2")
+        var list2 = document.getElementById("list2")
+        
+        
+        campo2.value= parseInt(campo2.value) - 1;
+        preco2.value= parseInt(preco2.value) - 23;
+        list2.value = parseInt(list2.value) - 1;
+    }
+
+    function AumentaPerc3() {
+
+        var campo3 = document.getElementById("percentual3")
+        var preco3 = document.getElementById("price3")
+        var list3 = document.getElementById("list3")
+    
+        campo3.value= parseInt(campo3.value) + 1;
+        preco3.value= parseInt(preco3.value) + 12;
+        list3.value = parseInt(list3.value) + 1;
+    }
+
+    function DiminuiPerc3() {
+    
+        var campo3 = document.getElementById("percentual3")
+        var preco3 = document.getElementById("price3")
+        var list3 = document.getElementById("list3")
+    
+        campo3.value= parseInt(campo3.value) - 1;
+        preco3.value= parseInt(preco3.value) - 12;
+        list3.value = parseInt(list3.value) - 1;
+    }
+
+    function AumentaPerc4() {
+
+        var campo4 = document.getElementById("percentual4")
+        var preco4 = document.getElementById("price4")
+        var list4 = document.getElementById("list4")
+    
+        campo4.value= parseInt(campo4.value) + 1;
+        preco4.value= parseInt(preco4.value) + 5;
+        list4.value = parseInt(list4.value) + 1;
+    }
+
+    function DiminuiPerc4() {
+    
+        var campo4 = document.getElementById("percentual4")
+        var preco4 = document.getElementById("price4")
+        var list4 = document.getElementById("list4")
+    
+        campo4.value= parseInt(campo4.value) - 1;
+        preco4.value= parseInt(preco4.value) - 5;
+        list4.value = parseInt(list4.value) - 1;
+    }
+
+    function AumentaPerc5() {
+
+        var campo5 = document.getElementById("percentual5")
+        var preco5 = document.getElementById("price5")
+        var list5 = document.getElementById("list5")
+    
+        campo5.value= parseInt(campo5.value) + 1;
+        preco5.value= parseInt(preco5.value) + 15;
+        list5.value = parseInt(list5.value) + 1;
+    }
+
+    function DiminuiPerc5() {   
+        var campo5 = document.getElementById("percentual5")
+        var preco5 = document.getElementById("price5")
+        var list5 = document.getElementById("list5")
+    
+        campo5.value= parseInt(campo5.value) - 1;
+        preco5.value= parseInt(preco5.value) - 15;
+        list5.value = parseInt(list5.value) - 1;
+    }
+
+    function AumentaPerc6() {
+
+        var campo6 = document.getElementById("percentual6")
+        var preco6 = document.getElementById("price6")
+        var list6 = document.getElementById("list6")
+    
+        campo6.value= parseInt(campo6.value) + 1;
+        preco6.value= parseInt(preco6.value) + 8;
+        list6.value = parseInt(list6.value) + 1;
+    }
+
+    function DiminuiPerc6() {   
+        var campo6 = document.getElementById("percentual6")
+        var preco6 = document.getElementById("price6")
+        var list6 = document.getElementById("list6")
+    
+        campo6.value= parseInt(campo6.value) - 1;
+        preco6.value= parseInt(preco6.value) - 8;
+        list6.value = parseInt(list6.value) - 1;
+    }
+    
+    // Função onde o botão irá abrir e fechar o container onde as informações estão armazenadas. 
+
+    // ---------------------------------------------------------------------------------------- //
+    // É atribuida uma classe à div pai, que possui o box que terá função de dropdown           //
+    // partir dai é estilizado de duas formas diferentes, uma para quando está ativa            //
+    // outra pra quando não.                                                                    //
+    // logo caso queira mudar, deve procurar no css onde está 'management_content.active'       //
+    // a animação é feita pelo css, js apenas coloca e retira a classe                          //
+    // ---------------------------------------------------------------------------------------- //
+
+    const grabButton = document.getElementsByClassName("button_title");
+    let box = document.getElementsByClassName("management_content");
+    let button_icon = document.getElementsByClassName("management_button");
+
+    for (let i = 0; i < grabButton.length; i++) {
+
+        grabButton[i].addEventListener(`click`, () => {
+            box[i].classList.toggle(`active`)  
+            if (box[i].classList.contains(`active`)) {
+                button_icon[i].innerText = "-"
+            }
+            else {
+                button_icon[i].innerText = "+"
+            }
+        });
+    }
 });
 
